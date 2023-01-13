959d68125e926ad0462f84556739a5df273e614e
Yeonho Seong <83576517+yeonho00@users.noreply.github.com>
2023-01-13 18:04:05 +0900

    BJ1463

---
+++ BaekJoon/BaekjoonProject/Class/BJ1436.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekjoonProject.Class
+{
+    class BJ1436
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            int curIdx = 0;
+            int curNum = 666;
+
+            while(curIdx < N)
+            {
+                //666 들어간 숫자마다 curIdx++
+                if (curNum.ToString().Contains("666"))
+                {
+                    curIdx++;
+                }
+                curNum++;
+            }
+
+            Console.WriteLine(--curNum);
+        }
+    }
+}
---
+++ BaekJoon/BaekjoonProject/Class/BJ1463.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekjoonProject.Class
+{
+    class BJ1463
+    {
+        static void Main()
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int N = int.Parse(Console.ReadLine());
+            int cnt = CntCalc(dic, N);
+
+            Console.WriteLine(cnt);
+        }
+
+        static int CntCalc(Dictionary<int, int> dic, int curNum)
+        {
+            int one, two, three;
+            
+            if(curNum % 3 == 0) three = curNum / 3;
+            if(curNum % 2 == 0) two = curNum / 2;
+            one = curNum - 1;
+
+            if(dic.Contains(three))
+        }
+    }
+}
